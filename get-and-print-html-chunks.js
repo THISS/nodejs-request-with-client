000a7585ac2dc15fd77b325273311f3078f418a0
Brenton Holswich <brenton.holswich@gmail.com>
2017-03-13 19:29:27 +0000

    my first attempt at getting and printing html chunks

---
+++ get-and-print-html-chunks.js
@@ -0,0 +1,32 @@
+// import the https module from nodejs
+let https = request('https');
+
+// Create a function that makes a request to the web and prints the results
+function getAndPrintHTMLChunks() {
+  const REQUEST_OPTIONS = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  let chunkCount = 0;
+
+  https.get(REQUEST_OPTIONS, (response) => {
+    // Set the encoding to read the data with the unicode format utf8 which is the encoding for the web default
+    response.setEncoding('utf8');
+
+    // When we hear the event 'data' emitted do our arrow function callback
+    response.on('data', (chunk) => {
+      chunkCount += 1;
+      console.log(chunk + '\r\n');
+    });
+
+    response.on('end', () => {
+      console.log(`There were ${chunkCount} chunks received.`);
+    });
+
+    response.on('error', (err) => {
+      console.log(`Error: ${err}`);
+    });
+  });
+
+}
