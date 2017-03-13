1809ba6f7c96b207378b92c1fc914d009cb0712a
Brenton Holswich <brenton.holswich@gmail.com>
2017-03-13 21:06:08 +0000

    completed the leetspeak activity by separating into helper function

---
+++ callbacks/print_1337.js
@@ -0,0 +1,26 @@
+// Get our HTML getter
+let getHtml = require('../http-functions');
+
+let requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step5.html'
+};
+
+let dictionary = {a: '4', e: '3', l: '1', o: '0', s: '5', t: '7', 'ck': 'x', 'er': '0r', 'you': 'j00'};
+
+// if the letter is in the dictionary then convert it
+function leetspeak(letter) {
+  if(letter in dictionary) {
+    return dictionary[letter];
+  }
+  return letter;
+}
+
+// Obfuscate with leetspeak
+function print1337 (html) {
+  let leetMapped = html.split('').map(leetspeak).join('');
+  console.log(leetMapped);
+}
+
+
+getHtml(requestOptions, print1337);
