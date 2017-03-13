1ebe866716fdebd10e2f98c7a48d3c2d8ebf1a0a
Brenton Holswich <brenton.holswich@gmail.com>
2017-03-13 21:34:19 +0000

    realised that the function I had didnt accomodate the two and three letter keys, looked up the string.replace function and implemented it

--- callbacks/print_1337.js
+++ callbacks/print_1337.js
@@ -18,10 +18,27 @@ function leetspeak(letter) {
 }
 
 // Obfuscate with leetspeak
+// Doesn't work because it deals with one letter at a time
 function print1337 (html) {
   let leetMapped = html.split('').map(leetspeak).join('');
   console.log(leetMapped);
 }
 
+// Spoke to don and he told me to look into .replace();
+// it is faster than doing what I was doing above
+function print1337Regex(html) {
+  let leetspeak = html.replace(/a/g, '4')
+  .replace(/you/g, 'j00')
+  .replace(/er/g, '0r')
+  .replace(/ck/g, 'x')
+  .replace(/e/g, '3')
+  .replace(/l/g, '1')
+  .replace(/o/g, '0')
+  .replace(/s/g, '5')
+  .replace(/t/g, '7');
 
-getHtml(requestOptions, print1337);
+    console.log(leetspeak);
+}
+
+
+getHtml(requestOptions, print1337Regex);
